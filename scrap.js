f5c456448e82423ae47fc9053380ea95f86e45a1
bryantbello <bryant.bello@outlook.com>
2016-05-24 21:15:45 -0400

    cheerio test

---
+++ scrap.js
@@ -0,0 +1,19 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var link = "https://www.reddit.com/r/" + process.argv[2];
+
+request(link, function(error, response, html) {
+    console.log(html);
+    var $ = cheerio.load(html);
+    var result = [];
+    $('p.title').each(function(i, element) {
+        var title = $(this).text();
+        var link = $(element).children().attr('href');
+
+        result.push({
+            title: title,
+            link: link
+        })
+    })
+    console.log(result);
+})
